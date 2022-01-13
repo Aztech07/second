d41ae170099a032f53f3052d623b60f1abb4606f
Sudhir Gupta <sid00230@gmail.com>
2022-01-13 16:23:02 +0530

    reset the code

--- pascaltriangle.cpp
+++ pascaltriangle.cpp
@@ -1,10 +1,6 @@
 #include<iostream>
 using namespace std;
 
-int printPascalTr(int arr[2],int arr2[],int length){
-	length=3;
-	arr[2]={1,0};
-	arr2[]={}
-	length=length+1;
-
+int main(){
+	return 0;
 }
